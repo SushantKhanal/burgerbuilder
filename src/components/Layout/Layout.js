54025e04292503071f4cfd9d5706078d44c9b19e
Sushant Khanal <dreamerking114@gmail.com>
2019-04-13 14:50:55 +0545

    side drawer open and closes on click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import ToolBar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <ToolBar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        revealSideDrawer: false,
+        revealBackdrop: false,
+    }
+
+    removeSideDrawerHandler = () => {
+        this.setState({
+            revealSideDrawer: false,
+            revealBackdrop : false,
+        })
+    }
+
+    revealSideDrawerHandler = () => {
+        this.setState({
+            revealSideDrawer: true,
+            revealBackdrop: true,
+        });
+    }
+
+    render () {
+        return (
+            <Aux>
+                <ToolBar revealSideDrawer={this.revealSideDrawerHandler}/>
+                <SideDrawer revealed={this.state.revealSideDrawer} 
+                    open={this.state.revealBackdrop} 
+                    removeSideDrawer={this.removeSideDrawerHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }    
+
+}
+
+export default Layout;
 
